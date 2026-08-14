be6135f5c501226de4d4cab4bf05b99a8a65ae3a
Nguyễn Khánh Toàn <Toan.nk235847@sis.hust.edu.vn>
2026-08-14 17:00:01 +0700

    Update dump.cpp

--- dump.cpp
+++ dump.cpp
@@ -1,136 +1,59 @@
-// There is an airplane game which to avoid enemies and gather coins.
-
-// The game's map has height is N and width is 5 (5 ≤ N ≤ 12), but due to the limit of the screen, the gaming zone is 5x5. Below the gaming zone, there is control zone, which is one line at the bottom of the screen where airplane move.
-
-// At the start of game, the airplane locates at center point of control zone.
-
-// Game rule :
-
-//          - Movement of airplane can be one of 3 options : left - right - stay at current column
-
-//          - In each turn, after airplane move, game map will move down one line
-
-//          - There is an option to use Bomb : bomb can be used to destroy all enemies in gaming zone, after used, all enemies will disappear and all coins will remain in the map. Bomb can be used only one.
-
-//          - When airplane meets a cell with coin, number of coins collected will increase by 1, if it meet an enemy, number of coins will decrease by 1. If number of coins < 0 -> Game Over.
-
-// Given the map Nx5 with C is value of each cell (0: nothing, 1: coin, 2:enemy), find out the maximum amount of coins can be achieved after finishing the game. If the game can not be finished (Game Over), return -1.
-
-// [Input]
-
-// The first line is the total number of test cases T ( T <= 50)
-
-// The first line of each test case contain N, which is the height of the map, then the N lines following descript the map's data.
-
-// [Output]
-
-// The maximum number of coins that can be collected after finishing the game.
-
-// If the game can not be finished, print -1.
-
- 
-
- 
-
-// Ex:
-
-
-
-// Input
-
-// 2
-
-// 5
-
-// 1 1 0 0 0
-
-// 1 2 2 2 1
-
-// 1 1 2 2 1
-
-// 2 2 2 1 2
-
-// 2 2 0 2 0
-
-// 8
-
-// 2 0 2 0 2
-
-// 1 0 1 2 0
-
-// 0 0 0 2 1
-
-// 2 0 2 0 1
-
-// 1 2 1 2 0
-
-// 0 2 2 0 2
-
-// 2 1 1 2 2
-
-// 0 2 1 2 0
-
- 
-
-// Output
-
-// Case #1
-
-// 3
-
-// Case #2
-
-// 4
 #include <iostream>
 
 using namespace std;
 
-#define MAX_SIZE 20
-
 struct Point {
 	int row;
 	int col;
+	int time;
 };
 
-int N;
-int maxCoins;
-int grid[13][5];
-Point bombed[100];
+Point q[100000];
+int front, rear;
+void enQueue(Point p) {
+	rear++;
+	q[rear] = p;
+}
+
+Point deQueue() {
+	front++;
+	return q[front];
+}
+
+int grid[50][50];
+bool visited[50][50];
+int N, M;
+int SR, SC;
+int P;
+int dr[4] = {-1, 0, 1, 0};
+int dc[4] = {0, 1, 0, -1};
+
+void init() {
+	front = -1;
+	rear = -1;
+}
+
+bool isEmpty() {
+	return front == rear;
+}
+
+int pipe[8][4] = {
+	{0, 0, 0, 0},
+	{1, 1, 1, 1},
+	{1, 0, 1, 0},
+	{0, 1, 0, 1},
+	{1, 1, 0, 0},
+	{0, 1, 1, 0},
+	{0, 0, 1, 1},
+	{1, 0, 0, 1}
+};
+
+int oppPipe(int k) {
+	return (k + 2) % 4;
+}
 
 bool isValid(int r, int c) {
-	return r >= 0 && c >=0 && r < N && r < 5;
-}
-
-void dfs(int cur_row, int cur_col, int cur_coin, int bomb_count) {
-	if(bomb_count) {
-		int bombed_count = 0;
-		for(int i = cur_row; i >= cur_row - 4; i--) {
-			for(int j = 0; j <= 4; j++) {
-				if(isValid(i, j) && grid[i][j] == -1) {
-					bombed[bombed_count].row = i;
-					bombed[bombed_count].col = j;
-					bombed_count++;
-					grid[i][j] = 1;
-				}
-			}
-		}
-		dfs(cur_row, cur_col, cur_coin, 0);
-		for(int i = 0; i < bombed_count; i++) {
-			grid[bombed[bombed_count].row][bombed[bombed_count].col] = -1;
-		}
-	}
-	cur_coin += grid[cur_row][cur_col];
-	if(cur_coin < 0) return;
-	if(cur_row == 0) {
-		maxCoins = max(maxCoins, cur_coin);
-		return;
-	}
-	if(isValid(cur_row - 1, cur_col)) dfs(cur_row - 1, cur_col, cur_coin, bomb_count);
-	if(isValid(cur_row - 1, cur_col - 1)) dfs(cur_row - 1, cur_col - 1, cur_coin, bomb_count);
-	if(isValid(cur_row - 1, cur_col + 1)) dfs(cur_row - 1, cur_col + 1, cur_coin, bomb_count);
-}
-void reset() {
-	maxCoins = -9999999;	
+	return r >= 0 && c >= 0 && r < N && c < M;
 }
 
 int main() {
@@ -139,20 +62,44 @@ int main() {
 	int T;
 	cin >> T;
 	for(int tc = 1; tc <= T; tc++) {
-		reset();
-		cin >> N;
+		int cnt = 0;
+		cin >> N >> M >> SR >> SC >> P;
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < 5; j++) {
+			for(int j = 0; j < M; j++) {
 				cin >> grid[i][j];
-				if(grid[i][j] == 2) grid[i][j] = -1;
+				visited[i][j] = false;
 			}
 		}
-		dfs(N - 1, 2, 0, 1);	
-		dfs(N - 1, 3, 0, 1);
-		dfs(N - 1, 1, 0, 1);
-		cout << "Case #" << tc << '\n';
-		if(maxCoins >= 0) cout << maxCoins << '\n';
-		else cout << "-1\n";
+		Point start_pos;
+		start_pos.row = SR;
+		start_pos.col = SC;
+		start_pos.time = 1;
+		enQueue(start_pos);
+		cnt++;
+		visited[SR][SC] = true;
+		while(!isEmpty()) {
+			Point cur = deQueue();
+			int cur_row = cur.row;
+			int cur_col = cur.col;
+			int cur_time = cur.time;
+			if(grid[cur_row][cur_col] == 0) continue;
+			for(int i = 0; i < 4; i++) {
+				if(!pipe[grid[cur_row][cur_col]][i]) continue;
+				int next_row = cur_row + dr[i];
+				int next_col = cur_col + dc[i];
+				int next_time = cur_time + 1;
+				if((pipe[grid[next_row][next_col]][oppPipe(i)]) && (next_time <= P) && !visited[next_row][next_col]) {
+					Point next;
+					next.row = next_row;
+					next.col = next_col;
+					next.time = next_time;
+					enQueue(next);
+					visited[next_row][next_col] = true;
+					cnt++;
+				}
+			}
+		}
+		cout << "Case #" << tc << '\n' << cnt << '\n';
 	}
 	return 0;
 }
