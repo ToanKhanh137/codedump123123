1a942046652a2e61cc1a1d4752e2e50b9b6a8807
Nguyễn Khánh Toàn <Toan.nk235847@sis.hust.edu.vn>
2026-08-24 14:14:31 +0700

    Update leetcode149.cpp

--- leetcode149.cpp
+++ leetcode149.cpp
@@ -3,23 +3,23 @@ class Solution {
 public:
 
     int maxPoints(vector<vector<int>>& points) {
-        map<pair<int, int>, int> m;
         int n = points.size();
         int maxP = 0;
         for(int i = 0; i < n - 1; i++) {
+            map<pair<int, int>, int> m;
             for(int j = i + 1; j < n; j++) {
                 int tu = points[i][0] - points[j][0];
                 int mau = points[i][1] - points[j][1];
-                if(tu < 0 && mau < 0) {
-                    tu = -tu;
-                    mau = -mau;
-                }
                 if(mau < 0) {
+                    tu = -tu;
                     mau = -mau;
+                }
+                else if(mau == 0 && tu < 0) {
                     tu = -tu;
                 }
-                tu = tu / gcd(tu, mau);
-                mau = mau / gcd(tu, mau);
+                int a = gcd(tu, mau);
+                tu = tu / a;
+                mau = mau / a;
                 m[{tu, mau}]++;
                 maxP = max(m[{tu, mau}], maxP);
             }
