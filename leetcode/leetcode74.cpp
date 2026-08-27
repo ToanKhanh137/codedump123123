d901e5e331fc0c011de226eb393c9e2360094f2d
Nguyễn Khánh Toàn <Toan.nk235847@sis.hust.edu.vn>
2026-08-27 10:23:07 +0700

    Create leetcode74.cpp

---
+++ leetcode/leetcode74.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/search-a-2d-matrix/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0;
+        int right = m*n - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            int idx = matrix[mid/m][mid%n];
+            if(idx == target) return true;
+            else if(idx > target) {
+                right = mid - 1;
+            } else left = mid + 1;
+        }
+        return false;
+    }
+};
